089ecbb1530ec730a0bec1e6a52ca99f52603a24
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-09-27 18:40:24 -0600

    refactorizando la configuracion de las variables de entonro

--- Backend/src/config/config.js
+++ Backend/src/config/config.js
@@ -2,16 +2,16 @@ import dotenv from 'dotenv'
 
 dotenv.config();
 
-export default{
+export default {
     persistence: process.env.PERSISTENCE,
-    port:process.env.PORT,
-    mongoUrl:process.env.URL_MONGODB,
+    port: process.env.PORT,
+    mongoUrl: process.env.URL_MONGODB,
     tokenKey: process.env.TOKEN_KEY,
-    clientIdGitHub:process.env.CLIENT_ID_GITHUB,
+    clientIdGitHub: process.env.CLIENT_ID_GITHUB,
     clienteSecretsGitHub: process.env.CLIENT_SECRETS_GITHUB,
     environment: process.env.ENVIRONMENT,
-    passwordGmail:process.env.PASSWORD_APP_GMAIL,
+    passwordGmail: process.env.PASSWORD_APP_GMAIL,
     correoGmail: process.env.CORREO_GMAIL,
     AppUrl: process.env.APP_URL,
-    tokenCookie:process.env.TOKEN_COOKIE
+    tokenCookie: process.env.TOKEN_COOKIE
 }
