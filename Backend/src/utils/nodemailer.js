0a66bba1eaa55ef127d007a164ae8de9f27add6a
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-09-30 18:04:00 -0600

    agregando dos entornos para enviar mensajes para test y para producion

--- Backend/src/utils/nodemailer.js
+++ Backend/src/utils/nodemailer.js
@@ -4,11 +4,21 @@ import config from '../config/config.js'
 console.log(config.userGmail)
 console.log(config.passwordGmail)
 
-export const transport = nodemailer.createTransport({
-    host: config.mailtrap_host,
-    port: 2525,
-    auth: {
+const configMail = {}
+
+if (config.environment === 'production') {
+    configMail.service = 'gmail'
+    configMail.auth = {
+        user: config.userGmail,
+        pass: config.passwordGmail
+    }
+} else {
+    configMail.host = config.mailtrap_host
+    configMail.port = 2525
+    configMail.auth = {
         user: config.mailtrap_user,
         pass: config.mailtrap_pass
     }
-})
+}
+
+export const transport = nodemailer.createTransport(configMail)
