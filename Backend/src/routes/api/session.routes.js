a656decc465eabcf63c2f5dcf60ba383f9c4cc04
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-10-01 10:45:14 -0600

    creando las dos rutas para autentificar por medio de google

--- Backend/src/routes/api/session.routes.js
+++ Backend/src/routes/api/session.routes.js
@@ -5,6 +5,7 @@ import { sessions } from "../../controllers/index.js";
 import { authorization } from "../../middlewares/authMiddleware.js";
 import { validateCreateLogin } from "../../utils/validator/login.js";
 import { validateCreateRegister } from "../../utils/validator/register.js";
+import * as passport from 'passport';
 
 const sessionRouter = Router();
 
@@ -15,5 +16,15 @@ sessionRouter.get("/logout", passportCall("jwt"), sessions.logout);
 sessionRouter.get("/current", passportCall("jwt"), sessions.current);
 sessionRouter.delete("/inactives", passportCall('jwt'), authorization('admin'), sessions.deleteInactives);
 
-sessionRouter.get("/auth/google", sessions.loginGoogle)
+sessionRouter.get("/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+)
+
+sessionRouter.get("/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  (req, res) => {
+    res.cookie("token", req.user.token, { httpOnly: true });
+    res.redirect("/");
+  }
+)
 export default sessionRouter;
