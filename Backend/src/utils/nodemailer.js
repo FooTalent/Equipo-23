442dff6090ff5d6e4e7791a83c4dc5848a0b1e53
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-10-01 10:23:10 -0600

    creando el envio de correo para producion

--- Backend/src/utils/nodemailer.js
+++ Backend/src/utils/nodemailer.js
@@ -1,16 +1,21 @@
 import nodemailer from 'nodemailer'
 import config from '../config/config.js'
 
-console.log(config.userGmail)
 console.log(config.passwordGmail)
+console.log(config.userGmail)
 
 const configMail = {}
 
 if (config.environment === 'production') {
     configMail.service = 'gmail'
+    configMail.port = 465 // 25 or 465 or 587
+    configMail.secure = true
     configMail.auth = {
-        user: config.userGmail,
+        user: config.userGmail, // apikey
         pass: config.passwordGmail
+    }
+    configMail.tls = {
+        rejectUnauthorized: false
     }
 } else {
     configMail.host = config.mailtrap_host
