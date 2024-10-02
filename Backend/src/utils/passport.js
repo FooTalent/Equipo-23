7a29bb93c3d5be9fe178ccfa2a544d00b775a104
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-10-01 18:31:35 -0600

    agregando la edad del user por medio del auth google passport

--- Backend/src/utils/passport.js
+++ Backend/src/utils/passport.js
@@ -4,7 +4,6 @@ import config from "../config/config.js";
 
 // passport google
 import GoogleStrategy from "passport-google-oauth20";
-import { generateAuthToken } from "./jwt.js";
 import { usersRepository } from "../repositories/index.js";
 
 const JWTStrategy = jwt.Strategy;
@@ -17,6 +16,7 @@ const cookieExtractor = (req) => {
   }
   return token;
 };
+
 
 const initializatePassport = () => {
   passport.use(
@@ -47,24 +47,19 @@ const initializatePassport = () => {
       async (accessToken, refreshToken, profile, done) => {
         try {
           console.log(profile);
-          const userExist = await usersRepository.getUserByEmail({ email: profile.emails[0].value });
+          const userExist = await usersRepository.getUserBy({ email: profile.emails[0].value });
           if (userExist) {
             return done(null, userExist);
           }
-
+          const age = calculateAge(profile.birthday);
           const user = {
             name: profile.displayName,
             email: profile.emails[0].value,
-            profile,
+            age: age,
           };
           usersRepository.createUser(user);
           const findUser = await usersRepository.getUserBy({ email: user.email });
-          const token = generateAuthToken(findUser);
-          res.cookie(config.tokenCookie, token, {
-            maxAge: 60 * 60 * 1000,
-            httpOnly: true,
-          });
-          return done(null, user);
+          return done(null, findUser);
         } catch (err) {
           console.log(err);
           return done(err);
