a1556974fd23e9a71a55b77d8987171d9d65b041
Juanes <juanespachon1203@gmail.com>
2024-10-03 12:34:14 -0500

    chore (env): add environment variables for apiUrl and integrate it in UserService

--- Frontend/minegocio/src/app/services/user.service.ts
+++ Frontend/minegocio/src/app/services/user.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { RegisterValues } from '../models/registerValues.model';
 import { LoginValues } from '../models/loginValues.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,10 @@ import { LoginValues } from '../models/loginValues.model';
 export class UserService {
 
   private http = inject(HttpClient);
+  private apiUrl = environment.apiUrl;
 
   registerUser(registerValues: RegisterValues) {
-    return this.http.post('http://localhost:3000/register', {
+    return this.http.post(`${this.apiUrl}/register`, {
       first_name: registerValues.firstName,
       last_name: registerValues.lastName,
       age: registerValues.age,
@@ -21,11 +23,11 @@ export class UserService {
   }
 
   verifyRegisterCode(code: string) {
-    return this.http.post('http://localhost:3000/verify-code', { code });
+    return this.http.post(`${this.apiUrl}/verify-code`, { code });
   }
 
   loginUser(loginValues: LoginValues) {
-    return this.http.post('http://localhost:3000/login', {
+    return this.http.post(`${this.apiUrl}/login`, {	
       email: loginValues.email,
       password: loginValues.password
     },{
