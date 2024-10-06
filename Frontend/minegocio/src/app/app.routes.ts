25f7fab28c87d8a05827fc244c1da186736d8028
Juanes <juanespachon1203@gmail.com>
2024-10-06 14:46:58 -0500

    feat(routes): Add all routes

--- Frontend/minegocio/src/app/app.routes.ts
+++ Frontend/minegocio/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { VerifyCodeComponent } from './pages/verify-code/verify-code.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: DashboardComponent},
+    {path: "login", component: LoginComponent},
+    {path: "verify-code", component: VerifyCodeComponent},
+    {path: "sign-up", component: SignUpComponent},
+];
