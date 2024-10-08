96e4b2522fbc0c3fb9d88231f74659b2759bf1e8
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-10-08 14:34:52 -0600

    seteando el los routes con los middlewares para manejar las imagnes con uplaod de multer

--- Backend/src/routes/api/products.routes.js
+++ Backend/src/routes/api/products.routes.js
@@ -2,16 +2,16 @@ import { Router } from 'express';
 import { products } from '../../controllers/index.js';
 import { authorization } from '../../middlewares/authMiddleware.js';
 import { passportCall, passportCallOptional } from '../../middlewares/passportMiddleware.js';
-import {validateCreateProduct} from '../../utils/validator/product.js'
-import { uploader } from '../../utils/multer.js';
+import { validateCreateProduct } from '../../utils/validator/product.js'
+import upload from '../../utils/multer.js';
 
 const productRouter = Router();
 
-productRouter.post('/', passportCall('jwt'),authorization('admin','vendor'), uploader.array('thumbnails', 5),validateCreateProduct,products.createProduct);
-productRouter.get('/', passportCallOptional('jwt'),products.getProducts);
-productRouter.get('/:pid', passportCallOptional('jwt'),products.getProductById);
-productRouter.put('/:pid',  passportCall('jwt'),authorization('admin','vendor'),products.updateProductById);
-productRouter.delete('/:pid',  passportCall('jwt'),authorization('admin','vendor'), products.deleteProductById);
-productRouter.post('/:pid/images',  passportCall('jwt'), authorization('vendor','admin'),uploader.array('thumbnails', 5), products.uploadProductImages);
+productRouter.post('/', passportCall('jwt'), authorization('admin', 'vendor'), upload.any('thumbnails', 5), validateCreateProduct, products.createProduct);
+productRouter.get('/', passportCallOptional('jwt'), products.getProducts);
+productRouter.get('/:pid', passportCallOptional('jwt'), products.getProductById);
+productRouter.put('/:pid', passportCall('jwt'), authorization('admin', 'vendor'), products.updateProductById);
+productRouter.delete('/:pid', passportCall('jwt'), authorization('admin', 'vendor'), products.deleteProductById);
+productRouter.post('/:pid/images', passportCall('jwt'), authorization('vendor', 'admin'), upload.array('thumbnails', 5), products.uploadProductImages);
 
 export default productRouter;
