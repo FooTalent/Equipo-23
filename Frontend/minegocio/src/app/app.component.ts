f23d4dcd1b2123b619b7849a69f921e5a49ea463
paola chaves <paochavess22@gmail.com>
2024-10-11 18:23:39 -0500

    version movil dasboard

--- Frontend/minegocio/src/app/app.component.ts
+++ Frontend/minegocio/src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'minegocio';
+
+
+
+
+
+  
 }
---
+++ Frontend/minegocio/src/app/components/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  /* menuVisible: boolean = false;
+
+  toggleMenu() {
+    this.menuVisible = !this.menuVisible;
+  }} */
+}
