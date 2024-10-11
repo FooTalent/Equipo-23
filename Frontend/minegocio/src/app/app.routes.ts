9210a7fb8e2a15a333a4438bfd22c9ee8e338ec2
Juanes <juanespachon1203@gmail.com>
2024-10-11 18:26:05 -0500

    feat(404): Add not-found-page component to app.routes

--- Frontend/minegocio/src/app/app.routes.ts
+++ Frontend/minegocio/src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { VerifyCodeComponent } from './pages/verify-code/verify-code.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 
 export const routes: Routes = [
     {path: "", component: DashboardComponent},
     {path: "login", component: LoginComponent},
     {path: "verify-code", component: VerifyCodeComponent},
     {path: "sign-up", component: SignUpComponent},
+
+
+    {path: "**", component: NotFoundPageComponent},
 ];
