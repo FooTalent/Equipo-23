359051c02317db52e4b43f02655dd9b7666eabbc
Juanes <juanespachon1203@gmail.com>
2024-10-11 22:42:11 -0500

    fix(sidebar): Avoid Sidebar in auth components

--- Frontend/minegocio/src/app/app.component.ts
+++ Frontend/minegocio/src/app/app.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { AuthService } from './services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent],
+  imports: [RouterOutlet, SidebarComponent, CommonModule],
   templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent {
+
+  private authService = inject(AuthService);
+
+  isLoggedIn = this.authService.isLoggedInSignal;
+
+}
