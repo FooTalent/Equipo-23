9f967b6d9cc88a8158b9d6ea7b197ce5657b6db5
Juanes <juanespachon1203@gmail.com>
2024-10-13 16:09:17 -0500

    feat(profile): Add editForm component

---
+++ Frontend/minegocio/src/app/components/user-edit-form/user-edit-form.component.ts
@@ -0,0 +1,41 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UserService } from '../../services/user.service';
+
+@Component({
+  selector: 'app-user-edit-form',
+  standalone: true,
+  imports: [ReactiveFormsModule, FormsModule, CommonModule],
+  templateUrl: './user-edit-form.component.html',
+  styleUrl: './user-edit-form.component.css'
+})
+export class UserEditFormComponent {
+
+  private userService = inject(UserService);
+
+  userEditForm = new FormGroup({
+    name: new FormControl('', [Validators.pattern(/^[a-zA-Z]+$/)]),
+    email: new FormControl('', [Validators.email]),
+    telefono: new FormControl(null,),
+    pais: new FormControl(''),
+    localidad: new FormControl('')
+  });
+
+  errorMessage: string = '';
+  isLoading = signal(false);
+
+  onEditSubmit(event: Event) {
+    event.preventDefault()
+
+  }
+
+  // Function to toggle form visibility
+
+  editFormOpen = this.userService.editFormOpen;
+
+  toggleEditForm() {
+    this.userService.toggleEditForm();
+  }
+
+}
--- Frontend/minegocio/src/app/pages/user-profile/user-profile.component.ts
+++ Frontend/minegocio/src/app/pages/user-profile/user-profile.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { UserEditFormComponent } from '../../components/user-edit-form/user-edit-form.component';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, UserEditFormComponent],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
 export class UserProfileComponent {
+
+  private userService = inject(UserService);
+
+  editFormOpen = this.userService.editFormOpen;
+
+  toggleEditForm() {
+    this.userService.toggleEditForm();
+  }
 
 }
