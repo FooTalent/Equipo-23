b43d82a33f8f42f06baf71842ed06e31ffa8aee7
German Soto <german.alejandrozulet@gmail.com>
2024-10-13 22:13:29 -0300

    config(tailwind): setting up colors

--- Frontend/minegocio/tailwind.config.js
+++ Frontend/minegocio/tailwind.config.js
@@ -1,15 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
       fontFamily: {
-        'nunito': ['Nunito', 'sans-serif'],
-      }
+        nunito: ["Nunito", "sans-serif"],
+      },
+      colors: {
+        primary: {
+          main: "#2B2738",
+          light: "#3B364C",
+        },
+        secondary: {
+          main: "#5D3CBA",
+        },
+        text: {
+          light: "#7C798A",
+        },
+        accent: "#E5E504",
+      },
     },
   },
   plugins: [],
-}
-
+};
