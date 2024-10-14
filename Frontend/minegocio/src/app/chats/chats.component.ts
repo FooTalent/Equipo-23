ad1defad0e128cf238f2d932cc357cdb6205fa68
German Soto <german.alejandrozulet@gmail.com>
2024-10-13 22:13:05 -0300

    feat(chat): get into conversation

--- Frontend/minegocio/src/app/chats/chats.component.ts
+++ Frontend/minegocio/src/app/chats/chats.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-chats',
   standalone: true,
   imports: [],
   templateUrl: './chats.component.html',
-  styleUrl: './chats.component.css'
+  styleUrls: ['./chats.component.css'],
 })
-export class ChatsComponent {
+export class ChatsComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit(): void {}
+
+  scrollToRight() {
+    const element = document.getElementById('messages-box');
+    if (element) {
+      element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'end',
+        inline: 'end',
+      });
+    }
+  }
+  scrollToLeft() {
+    const element = document.getElementById('messages-box');
+    if (element) {
+      element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'start',
+      });
+    }
+  }
 }
