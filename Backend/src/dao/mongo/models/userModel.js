e73109c15026522eac5c9c5196ab42f5c21a4325
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-10-15 17:18:01 -0600

    cambiando el nombre del campo surname -> lastName en el modelo user

--- Backend/src/dao/mongo/models/userModel.js
+++ Backend/src/dao/mongo/models/userModel.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema(
       minLength: 3,
       required: true,
     },
-    surnames: {
+    lastName: {
       type: String,
       required: false,
       default: ""
