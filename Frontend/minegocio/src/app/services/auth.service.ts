082c24ed6ed887bf28cda51edd98743a609263f3
Juanes <juanespachon1203@gmail.com>
2024-10-17 20:28:47 -0500

    Feat(auth): Add token expiration check

--- Frontend/minegocio/src/app/services/auth.service.ts
+++ Frontend/minegocio/src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable, signal } from '@angular/core';
 import { LoginValues } from '../models/loginValues.model';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +8,11 @@ import { HttpClient } from '@angular/common/http';
 export class AuthService {
 
   private http = inject(HttpClient);
+  private headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    Authorization: 'Bearer ' + localStorage.getItem('user_token')
+  });
 
   apiUrl = import.meta.env['NG_APP_API_URL']
   
@@ -22,11 +27,15 @@ export class AuthService {
   }
 
   setToken(token: string) {
-      localStorage.setItem('user_token', token);
+    localStorage.setItem('user_token', token);
   }
   
   removeToken() {
     localStorage.removeItem('user_token');
+  }
+
+  expirationCheck() {
+    return this.http.get(`${this.apiUrl}/api/sessions/current`, { headers: this.headers })
   }
 
   isLoggedIn() {
