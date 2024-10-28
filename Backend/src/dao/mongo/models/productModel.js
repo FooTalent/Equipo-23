b148b4f16f060d4863d04aaecee2b0e35dde9702
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-10-28 13:47:29 -0600

    agregando la propiedad status con los stados sale, restock, inventario, pending, delete

--- Backend/src/dao/mongo/models/productModel.js
+++ Backend/src/dao/mongo/models/productModel.js
@@ -56,6 +56,11 @@ const productSchema = new mongoose.Schema(
       type: String,
       default: "admin",
     },
+    status: {
+      type: String,
+      default: "sale",
+      enum: ["sale", "restock", "inventario", "pending", "deleted"],
+    },
   },
   {
     timestamps: true,
