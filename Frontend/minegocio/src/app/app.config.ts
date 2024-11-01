870fd354ec1709474a95f1e7cfd5a03e73c5f78e
paola chaves <paochavess22@gmail.com>
2024-11-01 12:45:14 -0500

    modificaciones finales por responsivo

--- Frontend/minegocio/src/app/app.config.ts
+++ Frontend/minegocio/src/app/app.config.ts
@@ -2,10 +2,12 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { HttpClientModule } from '@angular/common/http';
+import {provideAnimations} from "@angular/platform-browser/animations";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(HttpClientModule)
+    importProvidersFrom(HttpClientModule),
+    provideAnimations()
   ]
 };
--- Frontend/minegocio/src/app/components/dashboard/dashboard.component.ts
+++ Frontend/minegocio/src/app/components/dashboard/dashboard.component.ts
@@ -2,15 +2,83 @@ import { Component, NgModule } from '@angular/core';
 import { RouterLink } from "@angular/router";
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { single } from '../../data';
+import { NgIf } from '@angular/common';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+
+
+
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterLink,NgxChartsModule],
+  imports: [RouterLink,NgxChartsModule,NgIf ],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
+  view: [number, number] = [580, 290];
+  isMobile: boolean = window.innerWidth <= 768; // Ajusta el umbral para móvil
+
+   constructor() {
+    Object.assign(this, { single });
+    this.setViewSize(); // Configura el tamaño inicial
+  } 
+
+  onResize(event: any) {
+    this.isMobile = event.target.innerWidth <= 768; // Actualiza el estado móvil o escritorio
+    this.setViewSize();
+  }
+
+  setViewSize() {
+    if (this.isMobile) {
+      this.view = [340, 240]; // Tamaño para móvil
+    } else {
+      this.view = [600, 300]; // Tamaño para escritorio
+    }
+  }
+
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showXAxisLabel = true;
+  showYAxisLabel = true;
+
+  colorScheme = [
+    { name: 'Ene', value: '#e5e504' },
+    { name: 'Feb', value: '#ffffff' },
+    { name: 'Mar', value: '#5d3cba' },
+    { name: 'Abr', value: '#e5e504' },
+    { name: 'May', value: '#ffffff' },
+    { name: 'Jun', value: '#5d3cba' },
+    { name: 'Jul', value: '#e5e504' },
+    { name: 'Agos', value: '#ffffff' },
+    { name: 'Sep', value: '#5d3cba' },
+    { name: 'Oct', value: '#e5e504' },
+    { name: 'Nov', value: '#ffffff' },
+    { name: 'Dic', value: '#5d3cba' },
+  ];
+
+  onSelect(event: Event) {
+    console.log(event);
+  }
+
+  public single = [];
+
+  formatPercent(value: number): string {
+    return (value / 100).toLocaleString('en-US', {
+      style: 'percent',
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0
+    });
+  }
+
+  formatNumberAsPercent(value: number): string {
+    return Math.round(value) + '%';
+  }
+  
+}
+
+/* export class DashboardComponent {
   view: [number, number] = [580, 290];
   
 
@@ -26,7 +94,7 @@ export class DashboardComponent {
   showYAxisLabel = true;
 
 
-  colorScheme = [
+   colorScheme = [
     { name: 'Ene', value: '#e5e504' },   
     { name: 'Feb', value: '#ffffff' },   
     { name: 'Mar', value: '#5d3cba' },   
@@ -39,7 +107,7 @@ export class DashboardComponent {
     { name: 'Oct', value: '#e5e504' },  
     { name: 'Nov', value: '#ffffff' },   
     { name: 'Dic', value: '#5d3cba' },   
-  ];
+  ]; 
 
   constructor() {
     Object.assign(this, { single })
@@ -59,5 +127,7 @@ export class DashboardComponent {
   }
   formatNumberAsPercent(value: number): string {
     return Math.round(value) + '%'; 
-  }
-}
+  } 
+
+   
+} */
