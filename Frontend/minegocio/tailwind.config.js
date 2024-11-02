72ccb5ef19133c71263db3cd214771c4c19d18c0
German Soto <german.alejandrozulet@gmail.com>
2024-11-01 23:03:27 -0300

    styles(tailwindConfig): removing unnecesary css and adapting tailwind config

--- Frontend/minegocio/tailwind.config.js
+++ Frontend/minegocio/tailwind.config.js
@@ -19,7 +19,64 @@ module.exports = {
         },
         accent: "#E5E504",
       },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        glow: {
+          '0%': { backgroundColor: '#3b364c' },
+          '100%': { backgroundColor: '#5d3cba' },
+        },
+        'glow-border': {
+          '0%': { 
+            borderColor: '#3b364c',
+            borderWidth: '0'
+          },
+          '100%': { 
+            borderColor: '#7654d4',
+            borderWidth: '1.5px'
+          },
+        },
+        'scale-in-tr': {
+          '0%': {
+            transform: 'scale(0)',
+            transformOrigin: '100% 0%',
+            opacity: '1'
+          },
+          '100%': {
+            transform: 'scale(1)',
+            transformOrigin: '100% 0%',
+            opacity: '1'
+          }
+        }
+      },
+      animation: {
+        'fade-in': 'fadeIn 0.5s ease-in-out',
+        'glow-fast': 'glow 0.8s infinite alternate',
+        'glow-slow': 'glow 1.5s infinite alternate',
+        'glow-border': 'glow-border 1s infinite alternate',
+        'scale-in-tr': 'scale-in-tr 0.3s cubic-bezier(0.250, 0.460, 0.450, 0.940) both'
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    function({ addUtilities }) {
+      const newUtilities = {
+        '.no-arrows': {
+          '&::-webkit-inner-spin-button, &::-webkit-outer-spin-button': {
+            '-webkit-appearance': 'none',
+            'margin': '0'
+          },
+          '-moz-appearance': 'textfield',
+          'appearance': 'textfield'
+        },
+        '.chatbot-container': {
+          'scrollbar-width': 'none',
+          '-ms-overflow-style': 'none'
+        }
+      }
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    }
+  ],
 };
